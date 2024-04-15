79e91d5c8445dc950e0b23e15f9fb2d90b28685c
Daniel C <63077752+dcrep@users.noreply.github.com>
2024-04-14 20:17:04 -0400

    Animation 1 and 2 approaches

--- main.js
+++ main.js
@@ -13,5 +13,23 @@ const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-// const animation1 = alice1.animate(aliceTumbling, aliceTiming);
-// console.log(animation1);
+// Callback Hell
+/*
+let animation = alice1.animate(aliceTumbling, aliceTiming);
+animation.onfinish = alice2Tumble;
+
+function alice2Tumble(result) {
+  let animation = alice2.animate(aliceTumbling, aliceTiming);
+  animation.onfinish = alice3Tumble;
+}
+function alice3Tumble(result) {
+  alice3.animate(aliceTumbling, aliceTiming);
+}
+*/
+
+// Promise chain:
+/*
+alice1.animate(aliceTumbling, aliceTiming).finished.then(
+  (result) => alice2.animate(aliceTumbling, aliceTiming).finished.then(
+  (result) => alice3.animate(aliceTumbling, aliceTiming)));
+*/
